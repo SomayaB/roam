2a80943f76b6b87b259c9451a9d6cce6d11a06b5
SomayaB <somaya.bounouar@gmail.com>
2017-09-29 12:04:52 -0700

    Fixed some bugs. Added new post page and route. WIP

--- src/server/routes/posts.js
+++ src/server/routes/posts.js
@@ -3,17 +3,26 @@ const Posts = require('../../models/posts');
 const Users = require('../../models/users');
 const Cities = require('../../models/cities');
 
-router.get('/cities/:cityName/:postTitle', (request, response) => {
-  const city = request.params.cityName;
-  const postTitle = request.params.postTitle;
-  //search by what?? should it even be title or id?
-  Posts.getPostByTitle(postTitle)
-  .then(post => {
-    Posts.getNameFromUser(post.user_id)
-    .then(user => {
-      response.render('posts/show', {user, post});
-    });
-  });
+// router.get('/:author/:postTitle', (request, response) => {
+//   const author = request.params.author;
+//   const postTitle = request.params.postTitle;
+//   //search by what?? should it even be title or id?
+//   Posts.getPostByTitle(postTitle)
+//   .then(post => {
+//     console.log('post:::', post);
+//     Users.findById(post.user_id)
+//     .then(user => {
+//       response.render('posts/show', {user, post});
+//     });
+//   });
+// });
+
+router.get('/posts/new', (request, response) => {
+  response.render('posts/new');
 });
+
+// router.post('posts/new', (request, response) => {
+//
+// });
 
 module.exports = router;
--- src/server/utils.js
+++ src/server/utils.js
@@ -2,6 +2,10 @@ const createSession = (request, response, user) => {
   request.session.user = user;
 };
 
+// const renderError = (error, request, response) => {
+//   response.send(`Error: ${error.message}\n\n${error.stack}`);
+// };
+
 module.exports = {
   createSession
 };
