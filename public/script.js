da8099907b4b21b5a191c1757fadb0aaedb27d73
SomayaB <somaya.bounouar@gmail.com>
2017-10-13 15:41:09 -0700

    Upload profile picture functional but still need the ability to store in database.

--- public/script.js
+++ public/script.js
@@ -1,38 +1,51 @@
 
+  console.log('connected?');
+  //get elements
+  const editProfileButton = document.querySelector(".edit-profile-button");
+  const editProfileModal = document.querySelector(".edit-profile-modal");
+  const closeModalButton = document.querySelector(".close-modal");
 
-//get elements
-const editProfileButton = document.querySelector(".edit-profile-button");
-const editProfileModal = document.querySelector(".edit-profile-modal");
-const closeModalButton = document.querySelector(".close-modal");
-
-//add event listeners
-editProfileButton.addEventListener('click', () => {
-  editProfileModal.style.display = "block";
-});
-
-closeModalButton.addEventListener('click', () => {
-  editProfileModal.style.display = "none";
-});
-editProfileModal.addEventListener('click', () => {
-  if(event.target === editProfileModal) {
-    editProfileModal.style.display = "none";
-  }
-});
-
-//drop down comments
-const commentsButton = document.querySelector(".comments-button");
-const comments = document.querySelector(".comments");
-
-commentsButton.addEventListener('click', () => {
-  comments.classList.toggle('open-panel');
-});
-
-//edit comments
-const editCommentButtons = document.querySelectorAll(".edit-comment-button");
-
-editCommentButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    button.parentNode.parentNode.querySelector('.edit-comment-input').classList.toggle('show-edit-comment');
-    button.parentNode.parentNode.querySelector('.comment-paragraph').querySelector('.comment-paragraph-content').classList.toggle('hide-comment-paragraph');
+  //add event listeners Modal
+  editProfileButton.addEventListener('click', () => {
+    editProfileModal.style.display = "block";
   });
-});
+
+  closeModalButton.addEventListener('click', () => {
+    editProfileModal.style.display = "none";
+  });
+  editProfileModal.addEventListener('click', () => {
+    if(event.target === editProfileModal) {
+      editProfileModal.style.display = "none";
+    }
+  });
+
+
+  //drop down comments
+  const commentsButton = document.querySelector(".comments-button");
+  const comments = document.querySelector(".comments");
+
+  commentsButton.addEventListener('click', () => {
+    comments.classList.toggle('open-panel');
+  });
+
+  //edit comments
+  const editCommentButtons = document.querySelectorAll(".edit-comment-button");
+
+  editCommentButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      button.parentNode.parentNode.querySelector('.edit-comment-input').classList.toggle('show-edit-comment');
+      button.parentNode.parentNode.querySelector('.comment-paragraph').querySelector('.comment-paragraph-content').classList.toggle('hide-comment-paragraph');
+    });
+  });
+
+
+  // upload profile picture with uploader widget
+
+  // var uploadcare = require('uploadcare-widget');
+
+  var profilePic = document.querySelector('.profile-pic');
+  var widget = uploadcare.Widget('[role=uploadcare-uploader]');
+
+  widget.onUploadComplete(function (fileInfo) {
+    profilePic.src = fileInfo.cdnUrl;
+  });
